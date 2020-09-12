4e99a521fa58ac0f2df5f4afab4e8fad6f443d28
Raul Camacho <raulj.camcho@gmail.com>
2020-09-12 13:29:31 -0500

    Fix: Se cambia titulo peciente por pacientes

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
 
   return (
     <>
-      <h1>Administrador de Pecientes</h1>
+      <h1>Administrador de Pacientes</h1>
       <div className="container">
         <div className="row">
           <div className="one-half column">
